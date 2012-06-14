7927ca8ff11a0b5dd12213af2cd9937f9d5e8ac0
dirsot <dirsot@gmail.com>
2012-06-14 19:28:14 +0200

    start

---
+++ src/stresgui/StresGui.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stresgui;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author stive
+ */
+public class StresGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        mainForm window = new mainForm();
+        window.main(null);
+    }
+}
